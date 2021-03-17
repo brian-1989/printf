d419eb684bc2018705f90ea3a4f9f24e41533313
vicuartas230 <vicuartas230@gmail.com>
2021-03-17 20:44:18 +0000

    Fixing the int_min cases

--- sec_functions.c
+++ sec_functions.c
@@ -6,15 +6,10 @@
  * Return: The length of the number printed.
  */
 
-int print_number(int n)
+int print_number(unsigned int n)
 {
 	int i = 0;
 
-	if (n < 0)
-	{
-		n = -n;
-		_putchar('-');
-	}
 	if ((n / 10) != 0)
 	{
 		print_number(n / 10);
@@ -43,9 +38,12 @@ void print_i(va_list intgr, int *p_c)
 	int i = va_arg(intgr, int);
 
 	if (i < 0)
-		c = print_number(i) + 1;
-	else
-		c = print_number(i);
+	{
+		i = -i;
+		_putchar('-');
+		*(p_c) = *p_c + 1;
+	}
+	c = print_number(i);
 	*(p_c) = *p_c + c;
 }
 
